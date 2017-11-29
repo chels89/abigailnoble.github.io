2e7377a1808b0a014b0cae8a34d0081c066b7744
Abigail Noble <noble.abigail@gmail.com>
2017-11-29 17:16:44 -0500

    Drafted resume front page

---
+++ function.js
@@ -0,0 +1,9 @@
+//addEntry makes a new line with the to-do item, with functioning buttons
+function addEntry(value) {
+	var contactbutton = copy.querySelector(".contactbutton");
+	contactbutton.addEventListener("click",function(){
+		//want to include a function here that will ping an e-mail program and pull up a blank e-mail with abigail@abigail.science as the addressee//
+		list.removeChild(copy);
+	});
+}
+
